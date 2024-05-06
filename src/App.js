2828009b0fed7841ff992368d47ce22d2ba8c3e2
Carl Higgs <carlhiggs@gmail.com>
2024-05-07 00:49:35 +1000

    updated authenticator method to use gen2 aws react ui auth component (looks prettier with more straight forward code)

--- src/App.js
+++ src/App.js
@@ -4,30 +4,40 @@ import Navbar from './components/navbar.js';
 import Map from './components/map.js';
 import './App.css';
 import { Amplify } from 'aws-amplify';
-import { withAuthenticator, Button} from '@aws-amplify/ui-react';
+import { Authenticator  } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
-function App({ signOut }) {
+
+export default function App() {
   return (
-      <div className="App">
-      <Navbar/>
-      <Button onClick={signOut} className='sign-out'>Sign out</Button>
-      <Map/>
-      <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <button onClick={signOut} className='sign-out'>Sign out</button>
+          <div className="App">
+          <Navbar/>
+          <Map/>
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+        </div>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-export default withAuthenticator(App);
+
+
+
