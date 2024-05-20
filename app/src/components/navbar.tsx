d74603c028c459cb39588a60bdf0b8d5622c91f1
Carl Higgs <carlhiggs@gmail.com>
2024-05-21 00:38:49 +1000

    proof of concept nested grid layout

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react';
 import './navbar.css';
 import logo from './cropped_jibe_logo_rgb.png';
-import { Button, Flex, Tabs, Text } from '@aws-amplify/ui-react';
+import { Button, Card, Flex, Grid, Tabs, Text } from '@aws-amplify/ui-react';
 import Map from './map';
 
 const Navbar: FC = () => {
@@ -17,8 +17,23 @@ const Navbar: FC = () => {
             label: 'About',
             value: '1',
             content: (
-              <>
-<Flex justifyContent="space-between" direction="column">
+<Grid
+  columnGap="0.5rem"
+  rowGap="0.5rem"
+  templateColumns="1fr 1fr 1fr"
+  templateRows="3fr 1fr"
+>
+  <Card
+    columnStart="1"
+    columnEnd="2"
+  >
+    Nav
+  </Card>
+  <Card
+    columnStart="2"
+    columnEnd="-1"
+  >
+    <Flex justifyContent="space-between" direction="column">
 <Text>Supported by a VicHealth Impact Grant, we aim to catalyse the potential impact of results of a Victorian Department of Transport-funded RMIT/University of Cambridge collaborative study of the health impacts of urban and transport planning scenarios (https://cur.org.au/project/jibe/).  This will be achieved by creating a tool to enable knowledge translation, co-designed with stakeholder transport, planning and health policymakers, technical practitioners, and advocates. We will co-create a visualisation tool for our transport and health model results with stakeholders to support creating and implementing healthy built environment policies. The tool will provide policy-relevant evidence on built environment change scenarios that maximise active transport uptake, redress spatial and health inequities and inform investments in high impact-built environment interventions. This project will benefit from our well-established collaborations with government and non-government organisations facilitated through Healthy Liveable City Lab's Policy Advisory Group (PAG). The PAG is chaired by the CEO of Infrastructure Victoria and includes all responsible sectors for delivering a healthy built environment for all Melburnians, including Department of Transport and Planning.</Text>
  
  <Text>The VicHealth Impact Research Grant will facilitate the use of research models and evidence of the health and equity impacts of transport and planning strategies and policies to support the creation of healthy built environments for all Melbournians. We will address practice and policy needs related to the assessment of health and equity implications of key planning and transport visions in Victoria. We propose to co-design a visualisation tool to showcase the results of our transport and health model and share our modelling methods in a series of workshops. Co-design is a participatory process that involves stakeholders in the process of planning, design and implementation of a product (visualisation tool) (Javanparast et al., 2022).  This approach aligns with the AWS 'Working Backwards' methodology.</Text>
@@ -35,10 +50,18 @@ const Navbar: FC = () => {
  <Text>Developer: Carl Higgs</Text>
  <Text>Project officer: Atefeh Soleimani Roudi</Text>
  </Flex>
-                <Button isFullWidth onClick={() => setTab('2')}>
+                
+
+                </Card>
+  <Card
+    columnStart="2"
+    columnEnd="-1"
+  >
+    <Button isFullWidth onClick={() => setTab('2')}>
                   Visualise the 20 minute neighbourhood intervention for Melbourne
                 </Button>
-              </>
+  </Card>
+</Grid>
             ),
           },
           {
