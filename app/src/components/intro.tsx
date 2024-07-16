b9d6275f7bc6912582e6ac67863cf5589746c4be
Carl Higgs <carlhiggs@gmail.com>
2024-07-16 14:51:57 +1000

    added in the intro page

---
+++ app/src/components/intro.tsx
@@ -0,0 +1,27 @@
+
+import Typography from '@mui/material/Typography';
+import Stack from "@mui/material/Stack";
+import { Link } from '@mui/material';
+import { TransportHealthImpacts } from './vis/transporthealthimpacts.tsx';
+import { Ampersand } from './vis/transporthealthimpacts.tsx';
+
+export function Intro() {
+  return (
+    
+    <Stack
+      direction={{ xs: 'column', md: 'row' }}
+      justifyContent="center"
+      alignItems="center"
+      sx={{ width: 1, height: "90vh", overflow: 'hidden' }}
+    >
+          {TransportHealthImpacts("800","1076")}
+          <Stack
+      direction="column">
+          <Typography variant="h4" gutterBottom> An interactive platform to inform healthy transport planning policy and localised infrastructure interventions, and visualise the impacts of modelled transportation scenarios.</Typography>
+          <Typography variant="h6" gutterBottom><Link href="about/">Find out more</Link> about our platform and development roadmap.</Typography>
+          </Stack>
+          {Ampersand()}
+
+    </Stack>
+  );
+}
