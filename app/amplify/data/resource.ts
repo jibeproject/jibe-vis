d0f601f4923a63ca1aa3ff037fb48a9fdf3960e9
Carl Higgs <carlhiggs@gmail.com>
2024-08-16 23:15:25 +1000

    added resource.ts

---
+++ app/amplify/data/resource.ts
@@ -0,0 +1,23 @@
+import { a, defineData, ClientSchema} from '@aws-amplify/backend';
+
+const schema = a.schema({
+  Feedback: a.model({
+    id: a.string(),
+    comment: a.string(),
+    datetime: a.datetime(),
+    url: a.string()
+    })
+    .authorization( allow => [allow.publicApiKey().to(['create'])] )
+});
+
+// // Used for code completion / highlighting when making requests from frontend
+export type Schema = ClientSchema<typeof schema>;
+
+// defines the data resource to be deployed
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 }
+  }
+});
